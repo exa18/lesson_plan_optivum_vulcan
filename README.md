d474f34213415487bb48634673536b31a501ac00
exa18 <julian.cenkier@wp.eu>
2021-09-16 20:12:58 +0200

    init: Optivum lesson plan checker

---
+++ README.md
@@ -0,0 +1,42 @@
+#### LINUX
+# VULCAN's Optivum lesson plan
+## Checks and notify if change lesson plans.
+This is paid plan which buy polish schools at [Vulcan site](https://www.vulcan.edu.pl/programy) to generate lesson plans.
+
+## Config first
+Rename config file plancheckcfg to prepend with dot.
+### Mail
+```
+mailssl=
+mailsmtp='mailserver:port'
+mailfrom='mailfrom@adress'
+mailpass='password4mailfrom'
+mailto='mailto@adress'
+```
+mailssl : if no SSL just leave blank
+
+### Styling
+htmlstyle : it is preconfigured but if you want change for exp. colors then just edit
+
+### List
+```
+planlist=("ZS16;https://zs16.edu.bydgoszcz.pl/plan-lekcji?vsel=675&vsel2=;o32"
+"ZS16;;o19"
+";;o7"
+"ZS10;http://zs10.bydgoszcz.pl/html/plany/o4.html"
+)
+```
+Each entry in list consists of 2 to 3 items separated by a semicolon. The first example above is for new api and last is for old api.
+Leaving empty field, like in second and third, means use of last value.
+To get links for first just copy from adress bar, for last copy link from left panel.
+
+## Get links
+
+**2.0** Just copy from adress bar.
+![2.0](volcan_api_2.png)
+
+**1.0** Copy link from left panel (left click and Copy Link).
+![1.0](volcan_api_1.png)
+
+## Install and run
+Just put on any linux server or host with shell. Set execute flag on script with **chmod +x**. Set it to run as cron job.
---
+++ plancheck.sh
@@ -0,0 +1,88 @@
+#/bin/bash
+#
+#
+#	Checks and notify if change lesson plans
+#	valid for VULCAN's Optivum API generated
+#	arg: -m : eachtime send mail
+#
+#	created: julian.cenkier@wp.eu
+#	version: 20210915
+#
+#	Install on host with shell access
+#	and set cron job for period checks.
+#
+# >
+    SCRIPT=${0##*/}
+    SCRIPTname=${SCRIPT%.*}
+    cfg=".${SCRIPTname}cfg"
+    if [ -f $cfg ]; then
+        . $cfg
+    else
+        echo ".. Create and fill config: $cfg"
+        exit
+    fi
+
+        mailurls=
+        mailurlssl=
+    if [ ${#mailssl} -gt 0 ];then
+        mailurls='s'
+        mailurlssl=' --ssl-reqd'
+    fi
+
+    function sendMail(){
+        curl --url smtp$mailurls://$mailsmtp$mailurlssl \
+        --mail-from $mailfrom \
+        --mail-rcpt $mailto \
+        --user $mailfrom:$mailpass \
+        -T <(echo -en "From: ${mailfrom}\nTo: ${mailto}\nSubject: ${mailsub}\n${mailmsg}")
+    }
+
+	function checkit(){
+		curl -L "${pp_uri}${1}" > text
+		cu=$(cat text)
+		if [[ -n $1 ]];then
+			# New API
+			stab="<button id.*table>"
+			skl="font-size: 22px;\">(.)*</div>"
+		else
+			# Old
+			stab="</table.*table>"
+			skl="tytulnapis\">(.)*</span>"
+		fi
+		echo -e $cu | grep -Eo "${stab}" | grep -Po '<table.*$' > text1
+		cat text | grep -Eo "${skl}" | grep -Eo ">.*<" | cut -c2- | rev | cut -c2- | rev > text2
+			kl=$(cat text2)
+			msg=$(cat text1)
+			msgtit="<h3>${pp_prefix} / ${kl}</h3>"
+			text="plan_${pp_prefix}_${kl}"
+			textsum="${text}_sum"
+			cat text1 > $text
+				mailsub=$pp_prefix' PLAN '$kl' zmiana'
+				mailmsg='MIME-Version: 1.0\nContent-Type: text/html; charset=UTF-8\nContent-Transfer-Encoding: 8bit\n\n<html xmlns="http://www.w3.org/1999/xhtml"><head><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta name="x-apple-disable-message-reformatting"><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body>'$(echo "$htmlstyle")$(echo -e "$msgtit")$(echo -e "$msg")'</body></html>'
+		if [[ -e $textsum ]];then
+			sha256sum --check $textsum| grep -o 'FAILED' > textstatus
+			if [[ $(cat textstatus | wc -l) -gt 0 ]];then
+				sha256sum $text > $textsum
+				[[ -z $forcemail ]] && sendMail
+			fi
+		else
+			sha256sum $text > $textsum
+		fi
+			[[ -n $forcemail ]] && sendMail
+			rm textstatus text text1 text2
+	}
+# <
+#
+#
+forcemail=;[[ "${1}" = "-m" ]] && forcemail="yes"
+#
+if [[ ${#planlist[@]} -gt 0 ]];then
+	for i in "${planlist[@]}";do
+		IFS=';' read -ra plan <<< "$i"
+		[[ -n ${plan[0]} ]] && pp_prefix=$(echo "${plan[0]}" | xargs)
+		[[ -n ${plan[1]} ]] && pp_uri=$(echo "${plan[1]}" | xargs)
+		[[ -n $pp_prefix ]] && [[ -n $pp_uri ]] && checkit $(echo "${plan[2]}" | xargs)
+	done
+else
+	echo ".. in $cfg fill planlist"
+fi
