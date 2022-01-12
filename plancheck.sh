495f2c17ac6b2f78e126a4c8a99bf571a10c64c9
exa18 <julian.cenkier@wp.eu>
2022-01-12 01:09:20 +0100

    update: Optivum -> outputs cleaning with checking
    Added full html tags strip for downloaded files.
    Additional output html file.
    Added to output html which is send, checking changes and select them
    with class "change".  CSS optimized in config.

--- plancheck.sh
+++ plancheck.sh
@@ -7,7 +7,7 @@
 #	     -u : update only with no mail
 #
 #	created: julian.cenkier@wp.eu
-#	version: 20220105
+#	version: 20220112
 #
 #	Install on host with shell access
 #	and set cron job for period checks.
@@ -19,6 +19,10 @@
     cfg=".${SCRIPTname}cfg"
 	STARTdir="$(pwd)"
 	cd $SCRIPTdir || exit
+	#
+	forcemail=;[[ "${1}" = "-m" ]] && forcemail="yes"
+	justupdate=;[[ "${1}" = "-u" ]] && justupdate="yes"
+	#
     if [ -f $cfg ]; then
         . $cfg
     else
@@ -45,7 +49,6 @@
 
 	function checkit(){
 		curl -L "${pp_uri}${1}" > text
-		cu=$(cat text)
 		if [[ -n $1 ]];then
 			# New API
 			stab="<button id.*table>"
@@ -55,34 +58,97 @@
 			stab="</table.*table>"
 			skl="tytulnapis\">(.)*</span>"
 		fi
-		echo -e $cu | grep -Eo "${stab}" | grep -Po '<table.*$' | sed -e 's/<\/\?a\s*[^>]*>//g' > text1
+		echo -e $(cat text) | grep -Eo "${stab}" | grep -Po '<table.*$' | tr '\015' '|' | sed -e 's/<br*>/\*/g' | sed -e 's/<\/\?\s*[^>]*>//g' | tr '|' '\n' > text1
 		cat text | grep -Eo "${skl}" | grep -Eo ">.*<" | cut -c2- | rev | cut -c2- | rev > text2
 			kl=$(cat text2)
-			msg=$(cat text1)
-			msgtit="<h3>${pp_prefix} / ${kl}</h3>"
-			text="plan_${pp_prefix}_${kl}"
-			textsum="${text}_sum"
-			cat text1 > $text
-				mailsub=$pp_prefix' PLAN '$kl
-				mailmsg='MIME-Version: 1.0\nContent-Type: text/html; charset=UTF-8\nContent-Transfer-Encoding: 8bit\n\n<html xmlns="http://www.w3.org/1999/xhtml"><head><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta name="x-apple-disable-message-reformatting"><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body>'$(echo "$htmlstyle")$(echo -e "$msgtit")$(echo -e "$msg")'</body></html>'
+			readarray -t text2check <<< $(cat text1)
+			# trim spaces
+			for i in "${!text2check[@]}";do
+				a="${text2check[$i]}"
+				text2check[$i]=$(echo "${a}" | sed -e 's/^\s//g' | sed -e 's/\s$//g')
+			done
+			msgtit="<h3>${pp_prefix} / ${kl}</h3>"	# html titile (mail body)
+			mailsub=$pp_prefix' PLAN '$kl			# mail subject
+				#
+				# file names
+				#
+				text="plan_${pp_prefix}_${kl}"	# reference (file)
+				textsum="${text}_sum"			# hash
+				texthtml="${text}_html"			# html (mail body)
+			
+			readarray -t textorg <<< $(cat $text)	# read previews file
+			# here is place for make backup of previews file
+			echo "${text2check[@]}" > $text			# (over)write with downloaded file
+
 		if [[ -e $textsum ]];then
-			sha256sum --check $textsum| grep -o 'FAILED' > textstatus
-			if [[ $(cat textstatus | wc -l) -gt 0 ]];then
+			checkstatus=$(sha256sum --check $textsum | grep -o 'FAILED' | wc -l)
+			if [[ $checkstatus -gt 0 ]];then
 				sha256sum $text > $textsum
-				[[ -z $forcemail ]] && mailsub=$mailsub' zmiana' && sendMail
+				mailsub=$mailsub' zmiana'
+				#
+				# select changes
+				#
+				changed=()
+				for i in "${!text2check[@]}"; do
+					s="${textorg[$i]}"
+					c="${text2check[$i]}"
+					stat=
+					if [[ "$s" != "$c" ]];then
+						stat="!"	# changed
+						[[ -z $s ]] && [[ -n $c ]] && stat="+"	# added
+						[[ -n $s ]] && [[ -z $c ]] && stat="-"	# removed
+					fi
+					changed[$i]="${stat}"
+				done
 			fi
-			rm textstatus
 		else
 			sha256sum $text > $textsum
 		fi
 			rm text text1 text2
+			#
+			# create HTML template
+			#
+				day=7
+				type=("nr" "g" "l" "l" "l" "l" "l")	# cell type/class
+				i=0
+				msg=()
+				while [[ $i -lt ${#text2check[@]} ]] ;do
+					x=0
+					if [[ $i -lt $day ]];then
+						msg[$i]="<table class=\"opium_plan\"><tr>"
+						while [[ $x -lt $day ]];do
+							a="${msg[$i]}"
+							msg[$i]="${a}<th>${text2check[$i]}</th>"
+							x=$((x+1))
+							i=$((i+1))
+						done
+					else
+						msg[$i]="<tr>"
+						while [[ $x -lt $day ]];do
+							c="${changed[$i]}"
+							s=
+							[[ -n $c ]] && [[ $x -gt 1 ]] && s=" changed"
+							a="${msg[$i]}"
+							msg[$i]="${a}<td class=\"${type[$x]}${s}\">${text2check[$i]}</td>"
+							msg[$i]="$(echo ${msg[$i]} | sed -e 's/\*/<br>/g')"
+							x=$((x+1))
+							i=$((i+1))
+						done
+					fi
+						i=$((i-1))
+						msg[$i]="${msg[$i]}</tr>"
+					i=$((i+1))
+					[[ -z ${text2check[$i]} ]] && break
+				done
+				msg[$i]="</table>"
+				echo "${msg[@]}" | sed -e 's/>\s</></g' > $texthtml
+				msghtml="$(cat $texthtml)"
+			mailmsg='MIME-Version: 1.0\nContent-Type: text/html; charset=UTF-8\nContent-Transfer-Encoding: 8bit\n\n<html xmlns="http://www.w3.org/1999/xhtml"><head><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta name="x-apple-disable-message-reformatting"><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body>'$(echo "$htmlstyle")$(echo -e "$msgtit")$(echo -e "$msghtml")'</body></html>'
+			[[ $checkstatus -gt 0 ]] && [[ -z $forcemail ]] && sendMail
 			[[ -n $forcemail ]] && sendMail
 	}
 # <
 #
-#
-forcemail=;[[ "${1}" = "-m" ]] && forcemail="yes"
-justupdate=;[[ "${1}" = "-u" ]] && justupdate="yes"
 #
 if [[ ${#planlist[@]} -gt 0 ]];then
 	for i in "${planlist[@]}";do
