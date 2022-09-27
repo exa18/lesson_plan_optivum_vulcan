a98d98f5bd3e32ad3499c21380361f5fea651644
exa18 <julian.cenkier@wp.eu>
2022-09-27 23:35:05 +0200

    update: added INDEX
    Added making index.html with plans.

--- plancheck.sh
+++ plancheck.sh
@@ -7,7 +7,7 @@
 #	     -u : update only with no mail
 #
 #	created: julian.cenkier@wp.eu
-#	version: 20220913
+#	version: 20220927
 #
 #	Install on host with shell access
 #	and set cron job for period checks.
@@ -54,7 +54,7 @@
 	}
 
 	function checkit(){
-		curl -L "${pp_uri}${1}" > text
+		curl -kL "${pp_uri}${1}" > text
 		if [[ -n $1 ]];then
 			# New API
 			stab="<button id.*table>"
@@ -162,29 +162,29 @@
 				done
 				msg[$i]="</table>"
 			#
+			#	makeMail
 			#
 			msghtml=$(echo "${msg[@]}" | sed -e 's/>\s</></g')
 			mailmsg='MIME-Version: 1.0\nContent-Type: text/html; charset=UTF-8\nContent-Transfer-Encoding: 8bit\n\n<html xmlns="http://www.w3.org/1999/xhtml"><head><meta name="viewport" content="width=device-width, initial-scale=1.0"><meta name="x-apple-disable-message-reformatting"><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body>'$(echo "$htmlstyle")$(echo -e "$msgtit")$(echo -e "$msghtml")'</body></html>'
-
+			#
 			if [[ $checkstatus -gt 0 ]] || [[ -z $checkstatus ]];then
 				echo "${msghtml}" > $texthtml
-				makeWebPage
+				#
+				#	makeWebPage
+				#
+				[[ -n $htmlwww ]] && [[ -d ./www ]] && html='<!DOCTYPE html><html class="no-js" lang="pl"><head><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><meta name="viewport" content="width=device-width, initial-scale=1"><title>'$(echo "$pp_prefix :: $kl :: $d$dlast")'</title><meta http-equiv="content-type" content="text/html; charset=UTF-8"/>'$(echo "$htmlstyle")'<style>body{margin:0}.card {display:flex;justify-content:center;align-items:center;height:100vh;margin:auto}.card-body{overflow: scroll}.nochanges td.changed{background:transparent!important}.nochanges td b{display:none}.toggle{position: static;margin-bottom:0.5em;background:#ff0;border: none;padding: 0.5em 1.5em;text-align:center;text-decoration:none;display:inline-block;cursor:pointer;font-weight:bold}.nochanges button{background:#999}@media (max-width:58em) {body{font-size:80%}.card-body{height:100%}}</style><script>function toggleClass(){const element = document.querySelector(".card");element.classList.toggle("nochanges");}</script></head><body><div class="container"><div class="card nochanges"><div class="card-body"><button class="toggle" onclick="toggleClass()">'$(echo "$pp_prefix / $kl")'</button>'$(echo "$msghtml")'</div></div></div></body></html>' && echo "$html" > ./www/${pp_prefix}_${kl}.html
+				#
 				[[ -z $forcemail ]] && sendMail
 			fi
 			[[ -n $forcemail ]] && sendMail
-	}
-
-	function makeWebPage(){
-		if [[ -n $htmlwww ]];then
-			if [[ -e ./www ]];then
-				html='<!DOCTYPE html><html class="no-js" lang="pl"><head><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><meta name="viewport" content="width=device-width, initial-scale=1"><title>'$(echo "$pp_prefix :: $kl :: $d$dlast")'</title><meta http-equiv="content-type" content="text/html; charset=UTF-8"/>'$(echo "$htmlstyle")'<style>body{margin:0}.card {display:flex;justify-content:center;align-items:center;height:100vh;margin:auto}.card-body{overflow: scroll}.nochanges td.changed{background:transparent!important}.nochanges td b{display:none}.toggle{position: static;margin-bottom:0.5em;background:#ff0;border: none;padding: 0.5em 1.5em;text-align:center;text-decoration:none;display:inline-block;cursor:pointer;font-weight:bold}.nochanges button{background:#999}@media (max-width:58em) {body{font-size:80%}.card-body{height:100%}}</style><script>function toggleClass(){const element = document.querySelector(".card");element.classList.toggle("nochanges");}</script></head><body><div class="container"><div class="card nochanges"><div class="card-body"><button class="toggle" onclick="toggleClass()">'$(echo "$pp_prefix / $kl")'</button>'$(echo "$msghtml")'</div></div></div></body></html>'
-				echo "$html" > ./www/${pp_prefix}_${kl}.html
-			fi
-		fi
+			#
+			#	create html list
+			[[ -n $htmlwww ]] && [[ -e ./www/${pp_prefix}_${kl}.html ]] && indexitems="${indexitems}<a href="'"'"${pp_prefix}_${kl}.html"'"'"  class="'"'"list-group-item"'"'">$(cat ./www/${pp_prefix}_${kl}.html | grep -Eo '<title>.*</title>' | sed -e 's/<title>//g' | sed -e 's/<\/title>//g')</a>"
 	}
 # <
 #
 #
+indexitems=
 if [[ $d -gt ${planactv[0]} ]] && [[ $d -le ${planactv[1]} ]];then
 	if [[ ${#planlist[@]} -gt 0 ]];then
 		for i in "${planlist[@]}";do
@@ -193,6 +193,11 @@ if [[ $d -gt ${planactv[0]} ]] && [[ $d -le ${planactv[1]} ]];then
 			[[ -n ${plan[1]} ]] && pp_uri=$(echo "${plan[1]}" | xargs)
 			[[ -n $pp_prefix ]] && [[ -n $pp_uri ]] && checkit $(echo "${plan[2]}" | xargs)
 		done
+		#
+		#	makeIndexPage
+		#
+		[[ -n $htmlwww ]] && [[ -d ./www ]] && indexwww='<!DOCTYPE html><html class="no-js" lang="pl"><head><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><meta name="viewport" content="width=device-width, initial-scale=1"><title>Lesson Plan INDEX</title><meta http-equiv="content-type" content="text/html; charset=UTF-8"/><style>body{margin:0;background:#666}.card {display:flex;justify-content:center;align-items:center;height:100vh;margin:auto}.card-body{overflow: auto}.list-group-horizontal .list-group-item{margin-bottom:1em;background:#ff0;border: none;padding: 0.5em 1.5em;text-align:center;text-decoration:none;cursor:pointer;font-weight:bold;color:#000}.list-group-horizontal .list-group-item{display:block}</style></head><body><div class="container"><div class="card nochanges"><div class="list-group list-group-horizontal">'$(echo "$indexitems")'</div></div></div></body></html>' && echo "$indexwww" > ./www/index.html
+		#
 	else
 		echo ".. in $cfg fill planlist"
 	fi
